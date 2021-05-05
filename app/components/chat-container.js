1e06d7b5354eb9e46f575e03d7b8cfbcf7fc4703
John <john.dev.work.01@gmail.com>
2021-05-04 18:40:43 -0700

    Handling message submit with container / presentation component architecture

--- app/components/chat-container.js
+++ app/components/chat-container.js
@@ -1,11 +1,14 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 import fetch from 'fetch';
 
 export default class ChatContainerComponent extends Component {
   @tracked
   messages = [];
+
+  @service auth;
 
   @action
   async loadMessages() {
@@ -17,5 +20,27 @@ export default class ChatContainerComponent extends Component {
     // this.messages = [...this.messages, ...(await resp.json())];
     // would need to handle deduplication with above
     this.messages = await resp.json();
+  }
+
+  @action
+  async createMessage(body) {
+    const {
+      channel: { id: channelId, teamId },
+    } = this.args;
+
+    const userId = this.auth.currentUserId;
+
+    const resp = await fetch('/api/messages', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        teamId,
+        channelId,
+        userId,
+        body,
+      }),
+    });
   }
 }
